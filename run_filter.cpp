0da0c03058d8a7d2126ddba5cb8a25abc631a46a
robgineer <robgineer@gmail.com>
2021-04-03 23:15:37 +0200

    Input via args

--- run_filter.cpp
+++ run_filter.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
-#include "run_filter.h"
 #include <type_traits>
 #include "sensor_data_handler.h"
 #include <string>
 
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "usage: run_filter <file_path>\n";
+        return EXIT_FAILURE;
+    }
+    string file_path{ argv[1] };
+    
     SensorDataHandler data_handler;
-    data_handler.readSensorData(SENSOR_DATA);
+    data_handler.readSensorData(file_path);
 
-    // do cross check 
+    // do cross check
     cout << data_handler.sensor_data.ground_truth_x_values.size() << endl;
     cout << data_handler.sensor_data.ground_truth_y_values.size() << endl;
     cout << data_handler.sensor_data.sensor_1_x_values.size() << endl;
