f678211d825680c38659234b184d3e72f3553990
Yann Labou <Yann.Labou@gmail.com>
2021-04-04 10:27:32 +0200

    Ran clang-format

--- sensor_data_handler.cpp
+++ sensor_data_handler.cpp
@@ -1,32 +1,33 @@
+#include "sensor_data_handler.h"
 #include <cstdlib>
 #include <fstream>
-#include "sensor_data_handler.h"
 #include <string>
 
 using namespace std;
 
-void SensorDataHandler::readSensorData(string file_path) {
-    string ground_truth_x;
-    string sensor_1_x;
-    string ground_truth_y;
-    string sensor_1_y;
-    string line;
+void SensorDataHandler::readSensorData(string file_path)
+{
+  string ground_truth_x;
+  string sensor_1_x;
+  string ground_truth_y;
+  string sensor_1_y;
+  string line;
 
-    ifstream input_file_stream(file_path);
+  ifstream input_file_stream(file_path);
 
-    while( getline(input_file_stream, line)){
-        istringstream iss(line);
+  while (getline(input_file_stream, line)) {
+    istringstream iss(line);
 
-        // get current line values
-        getline(iss, ground_truth_x, ',');
-        getline(iss, sensor_1_x, ',');
-        getline(iss, ground_truth_y, ',');
-        getline(iss, sensor_1_y, ',');
+    // get current line values
+    getline(iss, ground_truth_x, ',');
+    getline(iss, sensor_1_x, ',');
+    getline(iss, ground_truth_y, ',');
+    getline(iss, sensor_1_y, ',');
 
-        // store values into corresponding vectors
-        sensor_data.ground_truth_x_values.push_back(std::strtod(ground_truth_x.c_str(), 0));
-        sensor_data.sensor_1_x_values.push_back(std::strtod(sensor_1_x.c_str(), 0));
-        sensor_data.ground_truth_y_values.push_back(std::strtod(ground_truth_y.c_str(), 0));
-        sensor_data.sensor_1_y_values.push_back(std::strtod(sensor_1_y.c_str(), 0));
-    }
+    // store values into corresponding vectors
+    sensor_data.ground_truth_x_values.push_back(std::strtod(ground_truth_x.c_str(), 0));
+    sensor_data.sensor_1_x_values.push_back(std::strtod(sensor_1_x.c_str(), 0));
+    sensor_data.ground_truth_y_values.push_back(std::strtod(ground_truth_y.c_str(), 0));
+    sensor_data.sensor_1_y_values.push_back(std::strtod(sensor_1_y.c_str(), 0));
+  }
 }
--- sensor_data_handler.h
+++ sensor_data_handler.h
@@ -1,21 +1,25 @@
 #include <sstream>
-#include <vector>
 #include <string>
+#include <vector>
 
 
-struct SensorData {
+struct SensorData
+{
   std::vector<double> ground_truth_x_values{};
   std::vector<double> sensor_1_x_values;
   std::vector<double> ground_truth_y_values;
   std::vector<double> sensor_1_y_values;
 };
 
-class SensorDataHandler {
-    public:
-        // sensor data incl. ground truth
-        SensorData sensor_data;
-        // reader
-        void readSensorData(std::string file_path);
-        // TODO
-        void writeSensorData(SensorData sensor_data);
+class SensorDataHandler
+{
+public:
+  // sensor data incl. ground truth
+  SensorData sensor_data;
+
+  // reader
+  void readSensorData(std::string file_path);
+
+  // TODO
+  void writeSensorData(SensorData sensor_data);
 };
